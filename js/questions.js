1216a4e75ceecae665215a2f87e038b23aa18b6a
Keishi Asai <isheik808@gmail.com>
2018-07-13 23:37:18 -0700

    first

---
+++ js/questions.js
@@ -0,0 +1,22 @@
+var questions = [
+  {
+    question: "Pineapple on Pizza?",
+    yes: 4,
+    no: 3
+  },
+  {
+    question: "Universal basic income?",
+    yes: 4,
+    no: 3
+  },
+  {
+    question: "Education is a Privlage?",
+    yes: 4,
+    no: 3
+  },
+  {
+    question: "University inportant?",
+    yes: 4,
+    no: 3
+  }
+];
